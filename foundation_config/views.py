a7343c098f3e66e31d3d4f0d51c4226c2fd1a002
Bartlomiej Mika <bart@mikasoftware.com>
2016-07-11 20:40:10 -0400

    Added a few improvements.

--- foundation_config/views.py
+++ foundation_config/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import login_required
 from django.utils import translation
 from django.core.urlresolvers import resolve, reverse
 from rest_framework import status
@@ -8,6 +9,7 @@ from foundation_public.decorators import group_required
 from foundation_public import constants
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_one_page(request):
     return render(request, 'foundation_config/organization/1_view.html',{
@@ -15,6 +17,7 @@ def config_org_step_one_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_two_page(request):
     return render(request, 'foundation_config/organization/2_view.html',{
@@ -22,6 +25,7 @@ def config_org_step_two_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_three_page(request):
     return render(request, 'foundation_config/organization/3_view.html',{
@@ -29,6 +33,7 @@ def config_org_step_three_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_four_page(request):
     return render(request, 'foundation_config/organization/4_view.html',{
@@ -36,6 +41,7 @@ def config_org_step_four_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_five_page(request):
     return render(request, 'foundation_config/organization/5_view.html',{
@@ -43,6 +49,7 @@ def config_org_step_five_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_six_page(request):
     return render(request, 'foundation_config/organization/6_view.html',{
@@ -50,6 +57,7 @@ def config_org_step_six_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_seven_page(request):
     return render(request, 'foundation_config/organization/7_view.html',{
@@ -57,6 +65,7 @@ def config_org_step_seven_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ORGANIZATION_ADMIN_GROUP_ID,])
 def config_org_step_eight_page(request):
     return render(request, 'foundation_config/organization/8_view.html',{
@@ -64,6 +73,7 @@ def config_org_step_eight_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 def config_entr_step_one_page(request):
     return render(request, 'foundation_config/entrepreneur/1_view.html',{
@@ -71,6 +81,7 @@ def config_entr_step_one_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 def config_entr_step_two_page(request):
     return render(request, 'foundation_config/entrepreneur/2_view.html',{
@@ -78,6 +89,7 @@ def config_entr_step_two_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 def config_entr_step_three_page(request):
     return render(request, 'foundation_config/entrepreneur/3_view.html',{
@@ -85,6 +97,7 @@ def config_entr_step_three_page(request):
     })
 
 
+@login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 def config_entr_step_four_page(request):
     return render(request, 'foundation_config/entrepreneur/4_view.html',{
--- foundation_public/decorators.py
+++ foundation_public/decorators.py
@@ -11,19 +11,23 @@ def tenant_required(view_func):
         if request.tenant.schema_name != "public" and request.tenant.schema_name != "test":
             return view_func(request, *args, **kwargs)
         else:
-            return HttpResponseForbidden(_("Missing tenant."))
+            return HttpResponseForbidden(
+                _("Missing tenant.")
+            )
     return wrapper
 
 
 def group_required(group_id_list):
     def decorator(func):
         def inner(request, *args, **kwargs):
-            for target_group_id in group_id_list:
-                for search_group in request.user.groups.all():
-                    if search_group.id == target_group_id:
-                        return func(request, *args, **kwargs)  # Return with success.
-
+            if request.user.is_authenticated():  # Only run if logged in.
+                for target_group_id in group_id_list:
+                    for search_group in request.user.groups.all():
+                        if search_group.id == target_group_id:
+                            return func(request, *args, **kwargs)  # Return with success.
             # Return error.
-            return HttpResponseForbidden(_("You do not belong to a particular group."))
+            return HttpResponseForbidden(
+                _("You do not belong to a particular group.")
+            )
         return inner
     return decorator
