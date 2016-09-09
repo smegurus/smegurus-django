9a2821f2dad8434a90729d21e397b13fae5a5a4a
Bartlomiej Mika <bart@mikasoftware.com>
2016-09-09 19:34:17 -0400

    Added step 5

--- tenant_intake/urls.py
+++ tenant_intake/urls.py
@@ -8,17 +8,18 @@ urlpatterns = (
     url(r'^intake/has_completed$', decorator_view.has_completed_intake_page, name='tenant_intake_has_completed'),
 
     # Entrepreneur - Round One
-    url(r'^intake/entrepreneur/round_1/step/1$', entrepreneur_view.intake_entr_round_one_step_one_page, name='tenant_intake_entr_round_one_step_one'),
-    url(r'^intake/entrepreneur/round_1/step/2$', entrepreneur_view.intake_entr_round_one_step_two_page, name='tenant_intake_entr_round_one_step_two'),
-    url(r'^intake/entrepreneur/round_1/step/3$', entrepreneur_view.intake_entr_round_one_step_three_page, name='tenant_intake_entr_round_one_step_three'),
-    url(r'^intake/entrepreneur/round_1/step/4$', entrepreneur_view.intake_entr_round_one_step_four_page, name='tenant_intake_entr_round_one_step_four'),
-    url(r'^intake/entrepreneur/round_1/step/5$', entrepreneur_view.intake_entr_round_one_step_five_page, name='tenant_intake_entr_round_one_step_five'),
+    url(r'^intake/entrepreneur/round_1/step-1$', entrepreneur_view.intake_entr_round_one_step_one_page, name='tenant_intake_entr_round_one_step_one'),
+    url(r'^intake/entrepreneur/round_1/step-2$', entrepreneur_view.intake_entr_round_one_step_two_page, name='tenant_intake_entr_round_one_step_two'),
+    url(r'^intake/entrepreneur/round_1/step-3$', entrepreneur_view.intake_entr_round_one_step_three_page, name='tenant_intake_entr_round_one_step_three'),
+    url(r'^intake/entrepreneur/round_1/step-4$', entrepreneur_view.intake_entr_round_one_step_four_page, name='tenant_intake_entr_round_one_step_four'),
+    url(r'^intake/entrepreneur/round_1/step-5$', entrepreneur_view.intake_entr_round_one_step_five_page, name='tenant_intake_entr_round_one_step_five'),
 
     # Entrepreneur - Round Two
-    url(r'^intake/entrepreneur/round_2/step/1$', entrepreneur_view.intake_entr_round_two_step_one_page, name='tenant_intake_entr_round_two_step_one'),
-    url(r'^intake/entrepreneur/round_2/step/2$', entrepreneur_view.intake_entr_round_two_step_two_page, name='tenant_intake_entr_round_two_step_two'),
-    url(r'^intake/entrepreneur/round_2/step/3$', entrepreneur_view.intake_entr_round_two_step_three_page, name='tenant_intake_entr_round_two_step_three'),
-    url(r'^intake/entrepreneur/round_2/step/4$', entrepreneur_view.intake_entr_round_two_step_four_page, name='tenant_intake_entr_round_two_step_four'),
+    url(r'^intake/entrepreneur/round_2/step-1$', entrepreneur_view.intake_entr_round_two_step_one_page, name='tenant_intake_entr_round_two_step_one'),
+    url(r'^intake/entrepreneur/round_2/step-2$', entrepreneur_view.intake_entr_round_two_step_two_page, name='tenant_intake_entr_round_two_step_two'),
+    url(r'^intake/entrepreneur/round_2/step-3$', entrepreneur_view.intake_entr_round_two_step_three_page, name='tenant_intake_entr_round_two_step_three'),
+    url(r'^intake/entrepreneur/round_2/step-4$', entrepreneur_view.intake_entr_round_two_step_four_page, name='tenant_intake_entr_round_two_step_four'),
+    url(r'^intake/entrepreneur/round_2/step-5$', entrepreneur_view.intake_entr_round_two_step_five_page, name='tenant_intake_entr_round_two_step_five'),
 
     # TODO: Implement.
     url(r'^intake/entrepreneur/finished$', entrepreneur_view.intake_round_one_finished_page, name='tenant_intake_finished'),
--- tenant_intake/views/entrepreneur_view.py
+++ tenant_intake/views/entrepreneur_view.py
@@ -188,6 +188,18 @@ def intake_entr_round_two_step_four_page(request):
 
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
+# @condition(last_modified_func=entrepreneur_func)
+@tenant_intake_has_completed_redirection_required
+def intake_entr_round_two_step_five_page(request):
+    intake, create = Intake.objects.get_or_create(me=request.tenant_me)
+    return render(request, 'tenant_intake/entrepreneur/round_2/5/view.html',{
+        'intake': intake,
+        'identities': IdentifyOption.objects.all()
+    })
+
+
+@login_required(login_url='/en/login')
+@group_required([constants.ENTREPRENEUR_GROUP_ID,])
 @condition(last_modified_func=entrepreneur_func)
 def intake_round_one_finished_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
