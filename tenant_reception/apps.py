374159588847b514f4975807a64c8423c2bd9369
Bartlomiej Mika <bart@mikasoftware.com>
2016-09-14 11:37:00 -0400

    Initial commitment to support pre-intake acceptance handling.  #114

---
+++ tenant_reception/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TenantReceptionConfig(AppConfig):
+    name = 'tenant_reception'
---
+++ tenant_reception/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from tenant_reception import views
+
+
+urlpatterns = (
+    # url(r'^progress$', views.progress_page, name='tenant_progress'),
+)
