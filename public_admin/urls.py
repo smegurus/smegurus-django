5aff039f3a2329a557aaa369148f274326c1b49b
Bartlomiej Mika <bart@mikasoftware.com>
2016-09-15 16:07:57 -0400

    Latest progress. #101

--- public_admin/urls.py
+++ public_admin/urls.py
@@ -3,6 +3,7 @@ from public_admin.views import dashboard_view, organization_view
 
 
 urlpatterns = (
+    url(r'^janitor/organization/create/(.*)/2/$', organization_view.organization_create_2_page, name='public_admin_organization_create_2'),
     url(r'^janitor/organization/create/(.*)/1/$', organization_view.organization_create_1_page, name='public_admin_organization_create_1'),
     url(r'^janitor/organization/initialize$', organization_view.organization_initialization_page, name='public_admin_organization_initialization'),
     url(r'^janitor/organization/list$', organization_view.organization_master_page, name='public_admin_organization_master'),
--- public_admin/views/organization_view.py
+++ public_admin/views/organization_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from foundation_public.models.organization import PublicOrganization
 from foundation_public.forms.userform import UserForm
@@ -6,6 +6,7 @@ from foundation_public.forms.loginform import LoginForm
 from foundation_public.forms.organizationform import PublicOrganizationForm
 from foundation_public.forms.postaladdressform import PublicPostalAddressForm
 from foundation_public.forms.contactpointform import PublicContactPointForm
+from foundation_public.models.postaladdress import PublicPostalAddress
 from foundation_public.models.countryoption import CountryOption
 from foundation_public.models.provinceoption import ProvinceOption
 from foundation_public.models.cityoption import CityOption
@@ -32,19 +33,27 @@ def organization_initialization_page(request):
 
 @staff_member_required
 def organization_create_1_page(request, id):
+    organization = get_object_or_404(PublicOrganization, pk=int(id))
+
+    # Generate address.
+    if organization.address == None:
+        organization.address = PublicPostalAddress.objects.create(
+            name='#' + str(id) + ' HQ Address'
+        )
+        organization.save()
+
+    # Return our code.
     return render(request, 'public_admin/organization/create/1/view.html',{
-        'org_form': PublicOrganizationForm(),
-        'address_form': PublicPostalAddressForm(),
-        'contact_form': PublicContactPointForm(),
-        'countries': CountryOption.objects.all()
+        'organization': organization,
+        'address_form': PublicPostalAddressForm(instance=organization.address),
     })
 
 
 @staff_member_required
-def organization_create_2_page(request):
+def organization_create_2_page(request, id):
+    organization = get_object_or_404(PublicOrganization, pk=int(id))
     return render(request, 'public_admin/organization/create/1/view.html',{
-        'org_form': PublicOrganizationForm(),
-        'address_form': PublicPostalAddressForm(),
-        'contact_form': PublicContactPointForm(),
-        'countries': CountryOption.objects.all()
+        # 'org_form': PublicOrganizationForm(),
+        # 'address_form': PublicPostalAddressForm(instance=organization.address),
+        'contact_form': PublicContactPointForm(instance=organization.contact_point),
     })
