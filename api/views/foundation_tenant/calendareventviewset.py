049e0579c1a4d9f915306faadff9923da50efa01
Bartlomiej Mika <bart@mikasoftware.com>
2016-09-22 18:32:31 -0400

    Added code for attending and not attending an event.

--- api/views/foundation_tenant/calendareventviewset.py
+++ api/views/foundation_tenant/calendareventviewset.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework import permissions
 from rest_framework import authentication
+from rest_framework import status
+from rest_framework import response
+from rest_framework.decorators import detail_route
 from api.pagination import LargeResultsSetPagination
 from api.permissions import IsOwnerOrIsAnEmployee
 from api.serializers.foundation_tenant  import CalendarEventSerializer
+from api.serializers.misc  import IntegerSerializer
 from foundation_tenant.models.calendarevent import CalendarEvent
 from foundation_tenant.models.me import TenantMe
 from foundation_tenant.models.tag import Tag
@@ -40,3 +44,33 @@ class CalendarEventViewSet(viewsets.ModelViewSet):
             for tag in calendar_event.tags.all():
                 me = TenantMe.objects.get(tags__id=tag.id)
                 calendar_event.pending.add(me)
+
+    @detail_route(methods=['put'], permission_classes=[permissions.IsAuthenticated])
+    def attending(self, request, pk=None):
+        """Will set the current authenticated User to be attending this event."""
+        try:
+            calendar_event = self.get_object()
+            calendar_event.pending.remove(request.tenant_me)
+            calendar_event.absentees.remove(request.tenant_me)
+            calendar_event.attendees.add(request.tenant_me)
+            return response.Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response(
+                data=str(e),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    @detail_route(methods=['put'], permission_classes=[permissions.IsAuthenticated])
+    def abstaining(self, request, pk=None):
+        """Will set the authenticated User to be abstaining from this event."""
+        try:
+            calendar_event = self.get_object()
+            calendar_event.pending.remove(request.tenant_me)
+            calendar_event.attendees.remove(request.tenant_me)
+            calendar_event.absentees.add(request.tenant_me)
+            return response.Response(status=status.HTTP_200_OK)
+        except Exception as e:
+            return response.Response(
+                data=str(e),
+                status=status.HTTP_400_BAD_REQUEST
+            )
