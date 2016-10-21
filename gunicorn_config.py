cac9fe9f1219342c68ef189ae6eca18b6c093fb6
Bartlomiej Mika <bart@mikasoftware.com>
2016-10-20 20:29:13 -0400

    Minor fix with our gunicorn script.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/usr/home/django/django-bizmula/env/bin/gunicorn'
-pythonpath = '/usr/home/django/django-bizmula'
+command = '/usr/home/django/smegurus-django/env/bin/gunicorn'
+pythonpath = '/usr/home/django/smegurus-django'
 bind = '127.0.0.1:8001'
 workers = 3
