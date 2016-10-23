32bb8d221007b97bdf88a669b412f21fd43d9c1e
Bartlomiej Mika <bart@mikasoftware.com>
2016-10-23 17:34:19 -0400

    Added dynamic table rows appending when uploading a new file. #175

--- foundation_tenant/models/fileupload.py
+++ foundation_tenant/models/fileupload.py
@@ -38,9 +38,10 @@ class TenantFileUpload(models.Model):
     )
 
     def __str__(self):
-        return str(self.datafile)
+        temp_filename = str(self.datafile)
+        return temp_filename.replace("upload/", "")
 
-    def get_s3_url(self): 
+    def get_s3_url(self):
         return "https://" + env_var('AWS_STORAGE_BUCKET_NAME') + ".s3.amazonaws.com/media/"+str(self.datafile)
 
     # def delete(self, *args, **kwargs):
