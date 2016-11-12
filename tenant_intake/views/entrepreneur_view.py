92d5c612c2d639a6d5e08a111aa2c3f3f14b1c5d
Bartlomiej Mika <bart@mikasoftware.com>
2016-11-12 11:35:59 -0500

    BUGFIX: Secondary intake process now works for Entrepreneur. #254

--- tenant_intake/views/entrepreneur_view.py
+++ tenant_intake/views/entrepreneur_view.py
@@ -7,7 +7,6 @@ from django.db.models import Q
 from django.core.urlresolvers import reverse
 from foundation_public.decorators import group_required
 from foundation_public.utils import latest_date_between
-from tenant_reception.decorators import tenant_reception_required
 from foundation_tenant.forms.intakeform import IntakeForm
 from foundation_tenant.models.base.naicsoption import NAICSOption
 from foundation_tenant.models.base.tag import Tag
@@ -36,7 +35,6 @@ def entrepreneur_func(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_one_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/1/view.html',{
@@ -49,7 +47,6 @@ def intake_entr_round_one_step_one_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_two_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/2/view.html',{
@@ -62,7 +59,6 @@ def intake_entr_round_one_step_two_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_three_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/3/view.html',{
@@ -75,7 +71,6 @@ def intake_entr_round_one_step_three_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_four_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/4/view.html',{
@@ -88,7 +83,6 @@ def intake_entr_round_one_step_four_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_five_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/5/view.html',{
@@ -100,7 +94,6 @@ def intake_entr_round_one_step_five_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_one_step_six_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_1/6/view.html',{
@@ -112,7 +105,6 @@ def intake_entr_round_one_step_six_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_two_step_one_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_2/1/view.html',{
@@ -124,7 +116,6 @@ def intake_entr_round_two_step_one_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_two_step_two_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_2/2/view.html',{
@@ -136,7 +127,6 @@ def intake_entr_round_two_step_two_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_two_step_three_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_2/3/view.html',{
@@ -148,7 +138,6 @@ def intake_entr_round_two_step_three_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_two_step_four_page(request):
     # Fetch the Intake object.
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
@@ -202,7 +191,6 @@ def intake_entr_round_two_step_four_page(request):
 @login_required(login_url='/en/login')
 @group_required([constants.ENTREPRENEUR_GROUP_ID,])
 # @condition(last_modified_func=entrepreneur_func)
-@tenant_reception_required
 def intake_entr_round_two_step_five_page(request):
     intake, create = Intake.objects.get_or_create(me=request.tenant_me)
     return render(request, 'tenant_intake/entrepreneur/round_2/5/view.html',{
