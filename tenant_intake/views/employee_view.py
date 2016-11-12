ed54b725c9e115c8bedc79445bb1839639d64ae8
Bartlomiej Mika <bart@mikasoftware.com>
2016-11-12 13:36:17 -0500

    Modified more GUI. #258

--- tenant_intake/views/employee_view.py
+++ tenant_intake/views/employee_view.py
@@ -35,6 +35,7 @@ def pending_intake_master_page(request):
     return render(request, 'tenant_intake/employee/master/view.html',{
         'page': 'intake-pending',
         'intakes': intakes,
+        'type': 'pending'
     })
 
 
@@ -57,6 +58,7 @@ def held_intake_master_page(request):
     return render(request, 'tenant_intake/employee/master/view.html',{
         'page': 'intake-held',
         'intakes': intakes,
+        'type': 'held'
     })
 
 
@@ -71,14 +73,40 @@ def held_intake_master_page(request):
 @tenant_configuration_required
 @tenant_intake_required
 @tenant_profile_required
-def intake_details_page(request, id):
+def pending_intake_details_page(request, id):
     intake = get_object_or_404(Intake, pk=id)
     advisors = TenantMe.objects.filter(owner__groups__id=constants.ADVISOR_GROUP_ID)
     tags = Tag.objects.filter(is_program=True)
     return render(request, 'tenant_intake/employee/details/view.html',{
-        'page': 'intake',
+        'page': 'intake-pending',
         'intake': intake,
         'form': IntakeForm(instance=intake),
         'advisors': advisors,
-        'tags': tags
+        'tags': tags,
+        'type': 'pending'
+    })
+
+
+@login_required(login_url='/en/login')
+@group_required([
+    constants.ADVISOR_GROUP_ID,
+    constants.ORGANIZATION_MANAGER_GROUP_ID,
+    constants.ORGANIZATION_ADMIN_GROUP_ID,
+    constants.CLIENT_MANAGER_GROUP_ID,
+    constants.SYSTEM_ADMIN_GROUP_ID,
+])
+@tenant_configuration_required
+@tenant_intake_required
+@tenant_profile_required
+def held_intake_details_page(request, id):
+    intake = get_object_or_404(Intake, pk=id)
+    advisors = TenantMe.objects.filter(owner__groups__id=constants.ADVISOR_GROUP_ID)
+    tags = Tag.objects.filter(is_program=True)
+    return render(request, 'tenant_intake/employee/details/view.html',{
+        'page': 'intake-held',
+        'intake': intake,
+        'form': IntakeForm(instance=intake),
+        'advisors': advisors,
+        'tags': tags,
+        'type': 'held'
     })
