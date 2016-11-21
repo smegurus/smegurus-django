c515aea437189cc8dbd47cf897976d80ac8c2a7d
Bartlomiej Mika <bart@mikasoftware.com>
2016-11-20 19:53:33 -0500

    Implemented question 13. #280

--- tenant_workspace/templatetags/tenant_workspace_tags.py
+++ tenant_workspace/templatetags/tenant_workspace_tags.py
@@ -4,6 +4,7 @@ from django import template
 from django.db.models import Q
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import get_object_or_404
 from foundation_tenant.utils import int_or_none
 from foundation_tenant.models.base.imageupload import TenantImageUpload
@@ -146,8 +147,8 @@ def render_question_008(workspace, module, node, question, answer):
 @register.inclusion_tag('templatetags/question/render_question_009.html')
 def render_question_009(workspace, module, node, question, answer):
     """
-    Question will render results based on previously saved answer from a
-    question that is formatted using "template_id" of 001.
+    DEPENDENCY:
+    - template #001 | QID: 10 | geographic market
     """
     # For this particular document and module, find the previous question.
     previous_question_id = int_or_none(question.dependency['previous_question_id'])
@@ -202,3 +203,56 @@ def render_question_011(workspace, module, node, question, answer):
         'imageupload': imageupload,
         "OTHER_TEXT": "Other (Please Specify)"
     }
+
+
+@register.inclusion_tag('templatetags/question/render_question_012.html')
+def render_question_012(workspace, module, node, question, answer):
+    """
+    DEPENDENCY:
+    - template #002 | QID: 02 | company name
+    - template #001 | QID: 10 | geographic market
+    - template #009 | QID: 11 | geographic market
+    """
+    # Convert JSON string into python dictionary.
+    picked = json.loads(answer.content)
+    OTHER_TEXT = "Other (Please Specify)"
+
+    # For this particular document and module, find the previous questions.
+    q1_qid = int_or_none(question.dependency['q1_qid'])
+    q2_qid = int_or_none(question.dependency['q2_qid'])
+    q3_qid = int_or_none(question.dependency['q3_qid'])
+    a1_raw = get_object_or_404(QuestionAnswer, question_id=q1_qid)
+    a1 = json.loads(a1_raw.content)
+    a2_raw = get_object_or_404(QuestionAnswer, question_id=q2_qid)
+    a2 = json.loads(a2_raw.content)
+    a3_raw = get_object_or_404(QuestionAnswer, question_id=q3_qid)
+    a3 = json.loads(a3_raw.content)
+
+    # Generate custom text from previous questions.
+    # 1. Generate company name.
+    company_name = a1['var_1']
+    has_short_name = bool(a1['var_2'])
+    if has_short_name:
+        company_name = a1['var_3']
+
+    # 2. Generate geographic info.
+    company_market = a3['var_1']
+
+    # 3. Generate text.
+    mission_statement = _("To provide customers of %(companyname)s in the %(companymarket)s market with the best possible value and customer experience, maintaining operational efficiency and providing a reasonable return for our shareholders and owners.") % {'companyname': company_name, 'companymarket': company_market}
+
+    # Render.
+    return {
+        'workspace': workspace,
+        'module': module,
+        'node': node,
+        'question': question,
+        'answer': answer,
+        'picked': picked,
+        "OTHER_TEXT": OTHER_TEXT,
+        'default_mission_statement': mission_statement
+    }
+
+#  If "No" selected: "To provide customers of (companyname) in the (companymarket)
+#  market with the best possible value and customer experience, maintaining operational
+#  efficiency and providing a reasonable return for our shareholders and owners.",
