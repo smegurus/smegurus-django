1a5c003cfafb399546b16b0b1dcc1a4f7750a6b9
Bartlomiej Mika <bart@mikasoftware.com>
2016-12-23 19:18:58 -0500

    Moved the code from tasks into a seperate call-command.

--- api/tasks.py
+++ api/tasks.py
@@ -1,8 +1,5 @@
 from celery import shared_task
-from foundation_public.models.organization import PublicOrganization
-from foundation_public.models.contactpoint import PublicContactPoint
-from foundation_public.models.postaladdress import PublicPostalAddress
-from foundation_public.models.organizationregistration import PublicOrganizationRegistration
+from django.core.management import call_command
 
 
 # DEVELOPERS NOTE:
@@ -14,46 +11,9 @@ def begin_organization_creation_task(registered_id):
     """
     Asynchronously create our tenant schema. Email owner when process completes.
     """
-    # Fetch our registered Organization.
-    try:
-        registered_org = PublicOrganizationRegistration.objects.get(id=int(registered_id))
-    except PublicOrganization.DoesNotExist:
-        return None
+    # Run the sub-routine for taking the OrganizationRegistration object
+    # creating our Tenant from it.
+    call_command('populate_organization', str(registered_id))  # foundation_public/management/commands/populate_organization.py
 
-    owner = registered_org.owner
-
-    # Create our associated models.
-    contact_point, created = PublicContactPoint.objects.get_or_create(owner=owner)
-    address, created = PublicPostalAddress.objects.get_or_create(owner=owner)
-
-
-    # Create our Tenant and have Django-Tenants create the schema for this
-    # Organization in our database.
-    org = PublicOrganization.objects.create(
-        owner=owner,
-        contact_point=contact_point,
-        address=address,
-        schema_name=registered_org.schema_name,
-        name=registered_org.name
-    )
-
-    # Perform a custom post-save action.
-    # Our tenant requires a domain so create it here.
-    from django.contrib.sites.models import Site
-    from foundation_public.models.organization import PublicDomain
-    domain = PublicDomain()
-    domain.domain = org.schema_name + '.' + Site.objects.get_current().domain
-    domain.tenant = org
-    domain.is_primary = False
-    domain.save()
-
-    # Override custom default values.
-    org.has_mentors = True
-    org.has_perks = True
-    org.is_setup = False
-
-    # Attach our current logged in User for our Organization.
-    org.users.add(owner)
-    org.save()
-
+    # Return nothing.
     return None
---
+++ foundation_auth/management/commands/send_organization_ready_email.py
@@ -0,0 +1,46 @@
+from django.core.signing import Signer
+from django.contrib.sites.models import Site
+from django.core.management.base import BaseCommand, CommandError
+from django.core.mail import send_mail
+from django.contrib.auth.models import User, Group
+from django.utils.translation import ugettext_lazy as _
+from smegurus import constants
+from smegurus.settings import env_var
+
+
+class Command(BaseCommand):
+    help = 'Command will send an email notifying the use that their organization has been built and is ready.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('email', nargs='+')
+
+    def handle(self, *args, **options):
+        for email in options['email']:
+            # Fetch the User account associated with this email.
+            try:
+                user = User.objects.get(email=email)
+                self.begin_processing(user)
+            except User.DoesNotExist:
+                pass
+
+    def begin_processing(self, user):
+        # Convert our User's ID into an encrypted value.
+        # Note: https://docs.djangoproject.com/en/dev/topics/signing/
+        signer = Signer()
+        id_sting = str(user.id).encode()
+        value = signer.sign(id_sting)
+
+        # Variables used to generate your output.
+        http_protocol = 'https://' if env_var("SECURE_SSL_REDIRECT") else 'http://'
+        url = http_protocol + '%s' % Site.objects.get_current().domain + '/en/password_reset/' + value + '/'
+        subject_text = 'Account Password Reset'
+        html_text = _('Click the following link to reset your password: %(url)s') % {'url': str(url)}
+
+        # Send the email.
+        send_mail(
+            subject_text,
+            html_text,
+            env_var('DEFAULT_FROM_EMAIL'),
+            [user.email],
+            fail_silently=False
+        )
