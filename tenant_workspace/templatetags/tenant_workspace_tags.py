086685999cb4c3262d6fc5029578ecdf1abb581a
Bartlomiej Mika <bart@mikasoftware.com>
2016-12-29 19:56:01 -0500

    Implemented #570

--- tenant_workspace/templatetags/tenant_workspace_tags.py
+++ tenant_workspace/templatetags/tenant_workspace_tags.py
@@ -993,6 +993,8 @@ def render_question_type_051(workspace, module, node, question, answer):
     - QID: 90 | Please list up to 5 licenses, registrations, or permits that you need to operate your business
     OR
     - QID: 114 | Please list all types of insurance, broken down from years 1-3, with details and costs for each.
+    OR
+    - QID: 116 | Please list your banking costs from years 1-3, with details and cost on each.
     """
     # For this particular document and module, find the previous questions.
     q1_qid = int_or_none(question.dependency['q1_qid'])
