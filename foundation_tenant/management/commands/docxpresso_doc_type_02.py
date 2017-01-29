09d8c8dcda1e4afb0aea3ab0498147565739ebee
Bartlomiej Mika <bart@mikasoftware.com>
2017-01-29 15:43:52 -0500

    Added semi-working code of generating documents.

--- foundation_tenant/management/commands/docxpresso_doc_type_02.py
+++ foundation_tenant/management/commands/docxpresso_doc_type_02.py
@@ -13,6 +13,7 @@ from foundation_tenant.models.bizmula.slide import Slide
 from foundation_tenant.models.bizmula.questionanswer import QuestionAnswer
 from foundation_tenant.models.base.me import TenantMe
 from foundation_tenant.models.base.fileupload import TenantFileUpload
+from foundation_tenant.models.base.naicsoption import NAICSOption
 from foundation_tenant.utils import int_or_none
 from smegurus import constants
 
@@ -74,8 +75,7 @@ class Command(BaseCommand):
         api_key_hashed = sha1_crypt.hash(api_key) #  (Deprecated: https://passlib.readthedocs.io/en/stable/lib/passlib.hash.sha1_crypt.html)
 
         # Generate our Docxpresso data to submit.
-        answers = QuestionAnswer.objects.filter(document=document)
-        docxpresso_data = self.generate_docxpresso_data(document, answers)
+        docxpresso_data = self.generate_docxpresso_data(document)
 
         # Generate our API call - Genere using Python dictonary.
         data = {
@@ -139,42 +139,99 @@ class Command(BaseCommand):
             self.style.SUCCESS(_('Finished importing Document #%s.') % str(document.id))
         )
 
-    def generate_docxpresso_data(self, document, answers):
-        print(answers)
-        return [
-            {
-                "var": "workspace_name",
-                "value": "Mika Software"
-            },
-            {
-                "var": "business_idea",
-                "value": "Mika Software"
-            },
-            {
-                "var": "naics_industry_name",
-                "value": "Information Technologies"
-            },
-            {
-                "var": "naics_industry_friendly_name",
-                "value": "Internet Apps"
-            },
-            {
-                "var": "years_of_exp",
-                "value": 1
-            },
-            {
-                "var": "research_source",
-                "value": ["Test 1", "Test 2"]
-            },
-            {
-                "var": "similar_business",
-                "value": ["Test 1", "Test 2", "Test 3"]
-            },
-            {
-                "var": "industry_contact",
-                "value": ["Test 1", "Test 2", "Test 3", "Test 4"]
-            }
-        ]
+    def generate_docxpresso_data(self, document):
+        docxpresso_data = []
+        answers = QuestionAnswer.objects.filter(document=document)
+        for answer in answers.all():
+            if answer.question.pk == 21: # workspace_name
+                docxpresso_data.append({
+                    "var": "workspace_name",
+                    "value": answer.content['var_1']
+                })
+
+            if answer.question.pk == 25: # naics_industry_name
+                naics_id = answer.content['var_5'] # Depth 5 NAICS ID
+                depth_five_naics = NAICSOption.objects.get(id=naics_id) # Get the name
+
+                docxpresso_data.append({
+                    "var": "naics_industry_name",
+                    "value": depth_five_naics.name
+                })
+                docxpresso_data.append({
+                    "var": "naics_industry_friendly_name",
+                    "value": answer.content['var_6']
+                })
+
+            if answer.question.pk == 26: # years_of_exp
+                docxpresso_data.append({
+                    "var": "years_of_exp",
+                    "value": answer.content['var_1']
+                })
+
+            if answer.question.pk == 27: # business_idea
+                docxpresso_data.append({
+                    "var": "business_idea",
+                    "value": answer.content['var_1']
+                })
+
+            if answer.question.pk == 28: # research_sources
+                arr = []
+                if answer.content['var_1_other']:
+                    arr.append(answer.content['var_1_other'])
+                else:
+                    if answer.content['var_1']:
+                        arr.append(answer.content['var_1'])
+
+                if answer.content['var_2_other']:
+                    arr.append(answer.content['var_2_other'])
+                else:
+                    if answer.content['var_2']:
+                        arr.append(answer.content['var_2'])
+
+                if answer.content['var_3_other']:
+                    arr.append(answer.content['var_3_other'])
+                else:
+                    if answer.content['var_3']:
+                        arr.append(answer.content['var_3'])
+
+                if answer.content['var_4_other']:
+                    arr.append(answer.content['var_4_other'])
+                else:
+                    if answer.content['var_4']:
+                        arr.append(answer.content['var_4'])
+
+                if answer.content['var_5_other']:
+                    arr.append(answer.content['var_5_other'])
+                else:
+                    if answer.content['var_5']:
+                        arr.append(answer.content['var_5'])
+
+                docxpresso_data.append({
+                    "var": "research_sources",
+                    "value": arr
+                })
+
+            if answer.question.pk == 29: # similar_businesses
+                arr = []
+                arr.append(answer.content['var_1'])
+                arr.append(answer.content['var_2'])
+                arr.append(answer.content['var_3'])
+                docxpresso_data.append({
+                    "var": "similar_businesses",
+                    "value": arr
+                })
+
+            if answer.question.pk == 30: # industry_contacts
+                arr = []
+                arr.append(answer.content['var_1'])
+                arr.append(answer.content['var_2'])
+                arr.append(answer.content['var_3'])
+                docxpresso_data.append({
+                    "var": "industry_contacts",
+                    "value": arr
+                })
+
+        return docxpresso_data
 
     def finalize_processing(self, document):
         pass
