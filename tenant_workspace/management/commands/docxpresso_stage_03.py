69a374d8ee41a5cbc3eb43ac76cc8bbca9b1d7c4
Bartlomiej Mika <bart@mikasoftware.com>
2017-01-30 15:16:29 -0500

    More progress with supporting docs.

--- tenant_workspace/management/commands/docxpresso_stage_03.py
+++ tenant_workspace/management/commands/docxpresso_stage_03.py
@@ -107,17 +107,83 @@ class Command(BaseCommand):
         # Generate our Docxpresso data to submit.
         docxpresso_data = self.get_docxpresso_data(workspace, document, answers)
 
+        data = {
+            "security": {
+                "publicKey": settings.DOCXPRESSO_PUBLIC_KEY,
+                "timestamp": timestamp,
+                "APIKEY": api_key_hashed
+            },
+            "template": "templates/stage3.odt",
+            "output": {
+                "format": suffix,
+                "response": "doc",
+                "name": stem
+            },
+            "replace": docxpresso_data
+        }
+
+        # We will convert our Python dictonary into a JSON diconary.
+        encoded_body = json.dumps(data).encode('utf-8')
+
+        # Send AJAX Post to Docxpresso server.
+        http = urllib3.PoolManager()
+        r = http.request(
+            'POST',
+            settings.DOCXPRESSO_URL,
+            # body=encoded_body,
+            fields={
+                "dataJSON": encoded_body
+            }
+            # headers={'Content-Type': 'application/json'}
+        )
+
+        doc_file = open('static/'+filename, 'wb')
+        doc_file.write(r.data)
+        doc_file.close()
+
+        # Open the saved file and create a file object associated with it
+        # and attach it to the document which will cause our file to be
+        # uploaded to the S3 instance.
+        from django.core.files import File
+        with open('static/'+filename, 'rb') as f:
+            # Create a new file upload and upload the data to a S3 instance.
+            docxpresso_file = TenantFileUpload.objects.create(
+                datafile = File(f),
+            )
+
+            # Fetch the document and then atomically modify it.
+            with transaction.atomic():
+                # Fetch the document.
+                document = self.get_document(workspace.id)
+
+                # If the file already exists then delete it from S3.
+                if document.docxpresso_file:
+                    document.docxpresso_file.delete()
+
+                # Generate our new file.
+                document.docxpresso_file = docxpresso_file
+                document.save()
+
+                # Delete the local file.
+                #TODO: Implement this.
+
     def get_docxpresso_data(self, workspace, document, answers):
         docxpresso_data = []
         for answer in answers.all():
             if answer.question.pk == 21: # business_name
                 docxpresso_data = self.do_q21(docxpresso_data, answer)
 
-            elif answer.question.pk == 27: # business_idea
-                docxpresso_data = self.do_q27(docxpresso_data, answer)
+            if answer.question.pk == 25: # naics_industry_friendly_name
+                docxpresso_data = self.do_q25(docxpresso_data, answer)
 
             elif answer.question.pk == 27: # business_idea
                 docxpresso_data = self.do_q27(docxpresso_data, answer)
+
+            elif answer.question.pk == 28: # research_sources
+                docxpresso_data = self.do_q28(docxpresso_data, answer)
+
+            elif answer.question.pk == 32: # product_categories
+                docxpresso_data = self.do_q32(docxpresso_data, answer)
 
             elif answer.question.pk == 33: # customer_type
                 docxpresso_data = self.do_q33(docxpresso_data, answer)
@@ -140,7 +206,7 @@ class Command(BaseCommand):
             elif answer.question.pk == 39: # geographic_market
                 docxpresso_data = self.do_q39(docxpresso_data, answer)
 
-            elif answer.question.pk == 40: # geographic_market
+            elif answer.question.pk == 40: # geographic_market | customer_buying_decisio
                 docxpresso_data = self.do_q40(docxpresso_data, answer)
 
             elif answer.question.pk == 74: # how_to_convince
@@ -149,9 +215,6 @@ class Command(BaseCommand):
             # customer_buying_decision 40
             # product_distribution ???
             # {{target_market_characteristics}} 49
-
-        # DEBUGGING PURPOSES
-        print(docxpresso_data)
 
         return docxpresso_data # Return our data.
 
@@ -164,12 +227,53 @@ class Command(BaseCommand):
         })
         return docxpresso_data
 
+    def do_q25(self, docxpresso_data, answer):
+        docxpresso_data.append({
+            "vars": [{
+                "var": "naics_industry_friendly_name",
+                "value": answer.content['var_6']
+            }]
+        })
+        return docxpresso_data
+
     def do_q27(self, docxpresso_data, answer):
         docxpresso_data.append({
             "vars": [{
                 "var": "business_idea",
                 "value": answer.content['var_1']
             }]
+        })
+        return docxpresso_data
+
+    def do_q28(self, docxpresso_data, answer):
+        arr = []
+        arr.append(answer.content['var_1'])
+        arr.append(answer.content['var_2'])
+        arr.append(answer.content['var_3'])
+        docxpresso_data.append({
+            "vars": [{
+                "var": "research_sources",
+                "value": arr
+            }],
+            "options": {
+                "element": "list"
+            }
+        })
+        return docxpresso_data
+
+    def do_q32(self, docxpresso_data, answer):
+        arr = []
+        arr.append(answer.content['var_1'])
+        arr.append(answer.content['var_2'])
+        arr.append(answer.content['var_3'])
+        docxpresso_data.append({
+            "vars": [{
+                "var": "product_categories",
+                "value": arr
+            }],
+            "options": {
+                "element": "list"
+            }
         })
         return docxpresso_data
 
@@ -209,40 +313,46 @@ class Command(BaseCommand):
         })
         return docxpresso_data
 
-    def do_q37(self, docxpresso_data, answer): #TODO LIST
-        print(answer.content)
-        print("\n")
-        # docxpresso_data.append({
-        #     "vars": [{
-        #         "var": "pestel_trends",
-        #         "value": []
-        #     }]
-        # })
+    def do_q37(self, docxpresso_data, answer):
+        # Get all our trends.
+        arr = []
+        for ans in answer.content:
+            arr.append(ans['var_3'])
+
+        # Generate our data and return it.
+        docxpresso_data.append({
+            "vars": [{
+                "var": "pestel_trends",
+                "value": arr
+            }]
+        })
         return docxpresso_data
 
-    def do_q38(self, docxpresso_data, answer): #TODO LIST
-        print(answer.content)
-        # print("\n")
-        # docxpresso_data.append({
-        #     "vars": [{
-        #         "var": "specific_sources",
-        #         "value": []
-        #     }]
-        # })
+    def do_q38(self, docxpresso_data, answer):
+        # Get all our trends.
+        arr = []
+        for ans in answer.content:
+            arr.append(ans['var_2'])
+
+        # Generate our data and return it.
+        docxpresso_data.append({
+            "vars": [{
+                "var": "specific_sources",
+                "value": arr
+            }]
+        })
         return docxpresso_data
 
-    def do_q39(self, docxpresso_data, answer): #TODO LIST
-        # print(answer.content)
-        # print("\n")
-        # docxpresso_data.append({
-        #     "vars": [{
-        #         "var": "geographic_market",
-        #         "value": []
-        #     }]
-        # })
+    def do_q39(self, docxpresso_data, answer):
+        docxpresso_data.append({
+            "vars": [{
+                "var": "geographic_market",
+                "value": answer.content['var_1_other'] if answer.content['var_1_other'] else answer.content['var_1']
+            }]
+        })
         return docxpresso_data
 
-    def do_q40(self, docxpresso_data, answer): #TODO LIST
+    def do_q40(self, docxpresso_data, answer): # customer_buying_decision | geographic_market
         # print(answer.content)
         # print("\n")
         # docxpresso_data.append({
@@ -254,4 +364,12 @@ class Command(BaseCommand):
         return docxpresso_data
 
     def do_q74(self, docxpresso_data, answer): #TODO Imp
+        # print(answer.content)
+        # print("\n")
+        docxpresso_data.append({
+            "vars": [{
+                "var": "how_to_convince",
+                "value": []
+            }]
+        })
         return docxpresso_data
