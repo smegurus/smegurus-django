7dd5f467e44cfa528a2d552757ee6bfb4fced5a0
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-03 23:58:28 -0500

    Implemented latest progress for supporting stage 8 docxpresso generator.

--- tenant_workspace/management/commands/docxpresso_stage_07.py
+++ tenant_workspace/management/commands/docxpresso_stage_07.py
@@ -142,7 +142,11 @@ class Command(BaseCommand):
 
     def set_answers(self, answers, api):
         for answer in answers.all():
-            if answer.question.pk == 61:
+            if answer.question.pk == 27: # business_idea
+                self.do_q27(answer, api)
+
+            elif answer.question.pk == 61:
+                # business_formal_name, business_friendly_name
                 self.do_q61(answer, api)
 
             elif answer.question.pk == 62:
@@ -219,67 +223,114 @@ class Command(BaseCommand):
 
             elif answer.question.pk == 154:
                 self.do_q154(answer, api)
+                
+    def do_q27(self, answer, api):
+        api.add_text("business_idea", answer.content['var_1'])
 
     def do_q61(self, answer, api):
-        # 61	7	{{business_formal_name}}
-        # 61	7	{{business_friendly_name}}
-        print("QID 61")
+        api.add_text('business_formal_name', answer.content['var_1'])
+        api.add_text('business_friendly_name', answer.content['var_3'])
 
     def do_q62(self, answer, api):
-        # 62	7	{{business_weaknesses}}
-        # 62	7	{{business_weakness_resolutions}}
-        print("QID 62")
+        business_weaknesses_array = []
+        business_weakness_resolutions_array = []
 
-    def do_q63(self, answer, api):
-        # 63	7	{{business_mission}}
-        print("QID 63")
+        # Populate rows.
+        for ans in answer.content:
+            business_weaknesses_array.append(ans['var_2'])
+            business_weakness_resolutions_array.append(ans['var_3'])
+
+        # Generate our custom item.
+        business_weaknesses_dict = {
+            "var": 'business_weaknesses',
+            'value': business_weaknesses_array
+        }
+        business_weakness_resolutions_dict = {
+            "var": 'business_weakness_resolutions',
+            'value': business_weakness_resolutions_array
+        }
+
+        # Generate the custom API query.
+        custom = {
+            "vars": [
+                business_weaknesses_dict,
+                business_weakness_resolutions_dict
+            ],
+            "options": {
+                "element": "table"
+            }
+        }
+
+        # --- Debugging purposes only. ---
+        # print(custom)
+
+        # Attach all out tables.
+        api.add_custom(custom)
+
+    def do_q63(self, answer, api): #BUG: NULL IS BEING RETURNED, MUST INVESTIGATE.
+        api.add_text('business_mission', answer.content['var_2'])
 
     def do_q64(self, answer, api):
-        # 64	7	{{business_vision}}
-        print("QID 64")
+        api.add_text('business_vision', answer.content['var_2'])
 
     def do_q65(self, answer, api):
         # 65	7	{{product_customer_need}}
-        print("QID 65")
+        print("QID 65") #TODO: IMPLEMENT
 
     def do_q66(self, answer, api):
         # 66	7	"{{business_how_different}}
-        print("QID 66")
+        print("QID 66") #TODO: IMPLEMENT
 
     def do_q67(self, answer, api):
         # 67	7	{{market_position}}
         # 67	7	{{market_position_details}}
-        print("QID 67")
+        print("QID 67") #TODO: IMPLEMENT
 
     def do_q68(self, answer, api):
         # 68	7	{{business_great_at}}
-        print("QID 68")
+        print("QID 68") #TODO: IMPLEMENT
 
     def do_q69(self, answer, api):
         # 69	7	{{pricing_strategy}}
-        print("QID 69")
+        print("QID 69") #TODO: IMPLEMENT
 
     def do_q70(self, answer, api):
         # 70	7	{{how_customer_buys}}
-        print("QID 70")
+        print("QID 70") #TODO: IMPLEMENT
 
     def do_q71(self, answer, api):
         # 71	7	{{distribution_challenge}}
         # 71	7	{{distribution_challenge_resolution}}
-        print("QID 71")
+        print("QID 71") #TODO: IMPLEMENT
 
     def do_q72(self, answer, api):
-        # 72	7	"{{key_success_factors}}
-        print("QID 72")
+        array = []
+
+        if answer.content['var_1_other']:
+            array.append(answer.content['var_1_other'])
+        else:
+            array.append(answer.content['var_1'])
+
+        if answer.content['var_2_other']:
+            array.append(answer.content['var_2_other'])
+        else:
+            array.append(answer.content['var_2'])
+
+        if answer.content['var_3_other']:
+            array.append(answer.content['var_3_other'])
+        else:
+            array.append(answer.content['var_3'])
+
+        api.add_text_paragraphs('key_success_factors', array)
 
     def do_q75(self, answer, api):
         # 75	7	{{customer_objections}}
         # 75	7	{{customer_objection_responses}}
-        print("QID 75")
+        print("QID 75") #TODO: IMPLEMENT
 
     def do_q76(self, answer, api):
         # 76	7	"{{customer_buying_time}}
-        print("QID 76")
+        print("QID 76") #TODO: IMPLEMENT
 
     def do_q77(self, answer, api):
         # 77	7	{{incentive_types}}
@@ -289,7 +340,7 @@ class Command(BaseCommand):
         # 77	7	{{incentive_y1_costs}}
         # 77	7	{{incentive_y2_costs}}
         # 77	7	{{incentive_y3_costs}}
-        print("QID 77")
+        print("QID 77") #TODO: IMPLEMENT
 
     def do_q78(self, answer, api):
         # 78	7	{{physical_marketing_types}}
@@ -299,7 +350,7 @@ class Command(BaseCommand):
         # 78	7	{{physical_marketing_y1_costs}}
         # 78	7	{{physical_marketing_y2_costs}}
         # 78	7	{{physical_marketing_y3_costs}}
-        print("QID 78")
+        print("QID 78") #TODO: IMPLEMENT
 
     def do_q79(self, answer, api):
         # 79	7	{{media_campaign_types}}
@@ -309,7 +360,7 @@ class Command(BaseCommand):
         # 79	7	{{media_campaign_y1_costs}}
         # 79	7	{{media_campaign_y2_costs}}
         # 79	7	{{media_campaign_y3_costs}}
-        print("QID 79")
+        print("QID 79") #TODO: IMPLEMENT
 
     def do_q80(self, answer, api):
         # 80	7	{{marketing_partnership_types}}
@@ -319,7 +370,7 @@ class Command(BaseCommand):
         # 80	7	{{marketing_partnership_y1_costs}}
         # 80	7	{{marketing_partnership_y2_costs}}
         # 80	7	{{marketing_partnership_y3_costs}}
-        print("QID 80")
+        print("QID 80") #TODO: IMPLEMENT
 
     def do_q104(self, answer, api):
         # 104	7	{{marketing_costs_m1}}
@@ -361,7 +412,7 @@ class Command(BaseCommand):
         # 104	7	{{marketing_costs_m35}}
         # 104	7	{{marketing_costs_m36}}
         # 104	7	{{marketing_costs_y3_total}}
-        print("QID 104")
+        print("QID 104") #TODO: IMPLEMENT
 
     def do_q142(self, answer, api):
         # 142	7	{{marketing_referral_types}}
@@ -370,7 +421,7 @@ class Command(BaseCommand):
         # 142	7	{{marketing_referral_y1_costs}}
         # 142	7	{{marketing_referral_y2_costs}}
         # 142	7	{{marketing_referral_y3_costs}}
-        print("QID 142")
+        print("QID 142") #TODO: IMPLEMENT
 
     def do_q143(self, answer, api):
         # 143	7	{{marketing_retention_types}}
@@ -379,25 +430,29 @@ class Command(BaseCommand):
         # 143	7	{{marketing_retention_y1_costs}}
         # 143	7	{{marketing_retention_y2_costs}}
         # 143	7	{{marketing_retention_y3_costs}}
-        print("QID 143")
+        print("QID 143") #TODO: IMPLEMENT
 
     def do_q147(self, answer, api):
-        # 147	7	{{business_strengths}}
-        print("QID 147")
+        array = []
+
+        for ans in answer.content:
+            array.append(ans['var_2'])
+
+        api.add_text_paragraphs('business_strengths', array)
 
     def do_q148(self, answer, api):
         # 148	7	{{business_opportunities}}
-        print("QID 148")
+        print("QID 148") #TODO: IMPLEMENT
 
     def do_q149(self, answer, api):
         # 149	7	{{business_threats}}
         # 149	7	{{business_threat_mitigations}}
-        print("QID 149")
+        print("QID 149") #TODO: IMPLEMENT
 
     def do_q153(self, answer, api):
         # 153	7	{{growth_strategies}}
-        print("QID 153")
+        print("QID 153") #TODO: IMPLEMENT
 
     def do_q154(self, answer, api):
         # 154	7	{{competitive_strategies}}
-        print("QID 154")
+        print("QID 154") #TODO: IMPLEMENT
