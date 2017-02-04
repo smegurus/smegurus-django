89dd497b42388e7ee8158a44708fe8f1560777d7
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-03 23:15:55 -0500

    Added placeholders for the stage 7 question.

--- tenant_workspace/management/commands/docxpresso_stage_07.py
+++ tenant_workspace/management/commands/docxpresso_stage_07.py
@@ -45,6 +45,8 @@ class Command(BaseCommand):
 
         self.begin_processing(workspace_id, api)
 
+        return #TODO: DELETE WHEN READY.
+
         # Return a success message to the console.
         self.stdout.write(
             self.style.SUCCESS(_('Finished processing stage 7 for workspace_id #%s.') % str(workspace_id))
@@ -140,9 +142,262 @@ class Command(BaseCommand):
 
     def set_answers(self, answers, api):
         for answer in answers.all():
-            pass
-    #         if answer.question.pk == 1:
-    #             self.do_q1(answer, api)
-    #
-    # def do_q1(self, answer, api):
-    #     api.add_text("self_assess_1", answer.content['var_1'])
+            if answer.question.pk == 61:
+                self.do_q61(answer, api)
+
+            elif answer.question.pk == 62:
+                self.do_q62(answer, api)
+
+            elif answer.question.pk == 63:
+                self.do_q63(answer, api)
+
+            elif answer.question.pk == 64:
+                self.do_q64(answer, api)
+
+            elif answer.question.pk == 65:
+                self.do_q65(answer, api)
+
+            elif answer.question.pk == 66:
+                self.do_q66(answer, api)
+
+            elif answer.question.pk == 67:
+                self.do_q67(answer, api)
+
+            elif answer.question.pk == 68:
+                self.do_q68(answer, api)
+
+            elif answer.question.pk == 69:
+                self.do_q69(answer, api)
+
+            elif answer.question.pk == 70:
+                self.do_q70(answer, api)
+
+            elif answer.question.pk == 71:
+                self.do_q71(answer, api)
+
+            elif answer.question.pk == 72:
+                self.do_q72(answer, api)
+
+            elif answer.question.pk == 75:
+                self.do_q75(answer, api)
+
+            elif answer.question.pk == 76:
+                self.do_q76(answer, api)
+
+            elif answer.question.pk == 77:
+                self.do_q77(answer, api)
+
+            elif answer.question.pk == 78:
+                self.do_q78(answer, api)
+
+            elif answer.question.pk == 79:
+                self.do_q79(answer, api)
+
+            elif answer.question.pk == 80:
+                self.do_q80(answer, api)
+
+            elif answer.question.pk == 104:
+                self.do_q104(answer, api)
+
+            elif answer.question.pk == 142:
+                self.do_q142(answer, api)
+
+            elif answer.question.pk == 142:
+                self.do_q142(answer, api)
+
+            elif answer.question.pk == 143:
+                self.do_q143(answer, api)
+
+            elif answer.question.pk == 147:
+                self.do_q147(answer, api)
+
+            elif answer.question.pk == 148:
+                self.do_q148(answer, api)
+
+            elif answer.question.pk == 153:
+                self.do_q153(answer, api)
+
+            elif answer.question.pk == 154:
+                self.do_q154(answer, api)
+
+    def do_q61(self, answer, api):
+        # 61	7	{{business_formal_name}}
+        # 61	7	{{business_friendly_name}}
+        print("QID 61")
+
+    def do_q62(self, answer, api):
+        # 62	7	{{business_weaknesses}}
+        # 62	7	{{business_weakness_resolutions}}
+        print("QID 62")
+
+    def do_q63(self, answer, api):
+        # 63	7	{{business_mission}}
+        print("QID 63")
+
+    def do_q64(self, answer, api):
+        # 64	7	{{business_vision}}
+        print("QID 64")
+
+    def do_q65(self, answer, api):
+        # 65	7	{{product_customer_need}}
+        print("QID 65")
+
+    def do_q66(self, answer, api):
+        # 66	7	"{{business_how_different}}
+        print("QID 66")
+
+    def do_q67(self, answer, api):
+        # 67	7	{{market_position}}
+        # 67	7	{{market_position_details}}
+        print("QID 67")
+
+    def do_q68(self, answer, api):
+        # 68	7	{{business_great_at}}
+        print("QID 68")
+
+    def do_q69(self, answer, api):
+        # 69	7	{{pricing_strategy}}
+        print("QID 69")
+
+    def do_q70(self, answer, api):
+        # 70	7	{{how_customer_buys}}
+        print("QID 70")
+
+    def do_q71(self, answer, api):
+        # 71	7	{{distribution_challenge}}
+        # 71	7	{{distribution_challenge_resolution}}
+        print("QID 71")
+
+    def do_q72(self, answer, api):
+        # 72	7	"{{key_success_factors}}
+        print("QID 72")
+
+    def do_q75(self, answer, api):
+        # 75	7	{{customer_objections}}
+        # 75	7	{{customer_objection_responses}}
+        print("QID 75")
+
+    def do_q76(self, answer, api):
+        # 76	7	"{{customer_buying_time}}
+        print("QID 76")
+
+    def do_q77(self, answer, api):
+        # 77	7	{{incentive_types}}
+        # 77	7	{{incentive_impacts}}
+        # 77	7	{{incentive_durations}}
+        # 77	7	{{incentive_cost_types}}
+        # 77	7	{{incentive_y1_costs}}
+        # 77	7	{{incentive_y2_costs}}
+        # 77	7	{{incentive_y3_costs}}
+        print("QID 77")
+
+    def do_q78(self, answer, api):
+        # 78	7	{{physical_marketing_types}}
+        # 78	7	{{physical_marketing_impacts}}
+        # 78	7	{{physical_marketing_uses}}
+        # 78	7	{{physical_marketing_cost_types}}
+        # 78	7	{{physical_marketing_y1_costs}}
+        # 78	7	{{physical_marketing_y2_costs}}
+        # 78	7	{{physical_marketing_y3_costs}}
+        print("QID 78")
+
+    def do_q79(self, answer, api):
+        # 79	7	{{media_campaign_types}}
+        # 79	7	{{media_campaign_impacts}}
+        # 79	7	{{media_campaign_durations}}
+        # 79	7	{{media_campaign_cost_types}}
+        # 79	7	{{media_campaign_y1_costs}}
+        # 79	7	{{media_campaign_y2_costs}}
+        # 79	7	{{media_campaign_y3_costs}}
+        print("QID 79")
+
+    def do_q80(self, answer, api):
+        # 80	7	{{marketing_partnership_types}}
+        # 80	7	{{marketing_partnership_impacts}}
+        # 80	7	{{marketing_partnership_durations}}
+        # 80	7	{{marketing_partnership_cost_types}}
+        # 80	7	{{marketing_partnership_y1_costs}}
+        # 80	7	{{marketing_partnership_y2_costs}}
+        # 80	7	{{marketing_partnership_y3_costs}}
+        print("QID 80")
+
+    def do_q104(self, answer, api):
+        # 104	7	{{marketing_costs_m1}}
+        # 104	7	{{marketing_costs_m2}}
+        # 104	7	{{marketing_costs_m3}}
+        # 104	7	{{marketing_costs_m4}}
+        # 104	7	{{marketing_costs_m5}}
+        # 104	7	{{marketing_costs_m6}}
+        # 104	7	{{marketing_costs_m7}}
+        # 104	7	{{marketing_costs_m8}}
+        # 104	7	{{marketing_costs_m9}}
+        # 104	7	{{marketing_costs_m10}}
+        # 104	7	{{marketing_costs_m11}}
+        # 104	7	{{marketing_costs_m12}}
+        # 104	7	{{marketing_costs_y1_total}}
+        # 104	7	{{marketing_costs_m13}}
+        # 104	7	{{marketing_costs_m14}}
+        # 104	7	{{marketing_costs_m15}}
+        # 104	7	{{marketing_costs_m16}}
+        # 104	7	{{marketing_costs_m17}}
+        # 104	7	{{marketing_costs_m18}}
+        # 104	7	{{marketing_costs_m19}}
+        # 104	7	{{marketing_costs_m20}}
+        # 104	7	{{marketing_costs_m21}}
+        # 104	7	{{marketing_costs_m22}}
+        # 104	7	{{marketing_costs_m23}}
+        # 104	7	{{marketing_costs_m24}}
+        # 104	7	{{marketing_costs_y2_total}}
+        # 104	7	{{marketing_costs_m25}}
+        # 104	7	{{marketing_costs_m26}}
+        # 104	7	{{marketing_costs_m27}}
+        # 104	7	{{marketing_costs_m28}}
+        # 104	7	{{marketing_costs_m29}}
+        # 104	7	{{marketing_costs_m30}}
+        # 104	7	{{marketing_costs_m31}}
+        # 104	7	{{marketing_costs_m32}}
+        # 104	7	{{marketing_costs_m33}}
+        # 104	7	{{marketing_costs_m34}}
+        # 104	7	{{marketing_costs_m35}}
+        # 104	7	{{marketing_costs_m36}}
+        # 104	7	{{marketing_costs_y3_total}}
+        print("QID 104")
+
+    def do_q142(self, answer, api):
+        # 142	7	{{marketing_referral_types}}
+        # 142	7	{{marketing_referral_impacts}}
+        # 142	7	{{marketing_referral_cost_types}}
+        # 142	7	{{marketing_referral_y1_costs}}
+        # 142	7	{{marketing_referral_y2_costs}}
+        # 142	7	{{marketing_referral_y3_costs}}
+        print("QID 142")
+
+    def do_q143(self, answer, api):
+        # 143	7	{{marketing_retention_types}}
+        # 143	7	{{marketing_retention_impacts}}
+        # 143	7	{{marketing_retention_cost_types}}
+        # 143	7	{{marketing_retention_y1_costs}}
+        # 143	7	{{marketing_retention_y2_costs}}
+        # 143	7	{{marketing_retention_y3_costs}}
+        print("QID 143")
+
+    def do_q147(self, answer, api):
+        # 147	7	{{business_strengths}}
+        print("QID 147")
+
+    def do_q148(self, answer, api):
+        # 148	7	{{business_opportunities}}
+        print("QID 148")
+
+    def do_q149(self, answer, api):
+        # 149	7	{{business_threats}}
+        # 149	7	{{business_threat_mitigations}}
+        print("QID 149")
+
+    def do_q153(self, answer, api):
+        # 153	7	{{growth_strategies}}
+        print("QID 153")
+
+    def do_q154(self, answer, api):
+        # 154	7	{{competitive_strategies}}
+        print("QID 154")
