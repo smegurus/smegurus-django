6224f15682c4255de7cd90016d0b5e5663a59e0c
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-11 14:32:38 -0500

    Minor fix to previous submission.

--- foundation_public/templatetags/foundation_public_tags.py
+++ foundation_public/templatetags/foundation_public_tags.py
@@ -31,8 +31,7 @@ def get_app_public_domain():
     Returns the full URL to the domain. The output from this function gets
     generally appended with a path string.
     """
-    http_protocol = 'http://'
-    return http_protocol + '%s' % Site.objects.get_current().domain
+    return settings.SMEGURUS_APP_HTTP_PROTOCOL + '%s' % settings.SMEGURUS_APP_HTTP_DOMAIN
 
 
 
@@ -41,13 +40,7 @@ def get_app_tenant_domain(organization):
     """
     Reverse the URL of the request + view name for this Organization.
     """
-    from django.contrib.sites.shortcuts import get_current_site # Reverse
-    from django.core.urlresolvers import resolve, reverse # Reverse
-    from django.contrib.sites.models import Site
-    from smegurus.settings import env_var
-
-    http_protocol = 'https://' if env_var("SECURE_SSL_REDIRECT") else 'http://'
     if organization.schema_name:
-        return http_protocol + organization.schema_name + '.%s' % Site.objects.get_current().domain
+        return settings.SMEGURUS_APP_HTTP_PROTOCOL + organization.schema_name + '.%s' % settings.SMEGURUS_APP_HTTP_DOMAIN
     else:
-        return http_protocol + '%s' % Site.objects.get_current().domain
+        return settings.SMEGURUS_APP_HTTP_PROTOCOL + '%s' % settings.SMEGURUS_APP_HTTP_DOMAIN
