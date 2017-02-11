83ef89e256909fb8e70ec1112e74d3c53ad23ebf
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-11 15:37:16 -0500

    BUGFIX: Enabled doc generation. #757

--- tenant_workspace/management/commands/docxpresso_stage_09.py
+++ tenant_workspace/management/commands/docxpresso_stage_09.py
@@ -92,8 +92,6 @@ class Command(BaseCommand):
         # Take our content and populate docxpresso with it.
         self.set_answers(workspace, answers, api)
 
-        return
-
         # Generate our document!
         doc_filename = api.get_filename()
         doc_bin_data = api.generate()
