9041cf305a498db71b912bd09ea0752c179e662b
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-21 02:04:54 -0500

    HOTFIX: Prevent divide by zero

--- tenant_workspace/templatetags/qt_065_tag.py
+++ tenant_workspace/templatetags/qt_065_tag.py
@@ -23,12 +23,23 @@ def matrix_subtract_by(arr1, arr2):
 
 
 def matrix_divide_by(arr1, arr2):
-    return {
-        'yr1': arr1['yr1'] / arr2['yr1'],
-        "yr2": arr1['yr2'] / arr2['yr2'],
-        "yr3": arr1['yr3'] / arr2['yr3'],
-        "total": arr1['total'] / arr2['total']
-    }
+    computation = {}
+    try:
+        computation['yr1'] = arr1['yr1'] / arr2['yr1'],
+    except Exception as e:
+        computation['yr1'] = 0
+
+    try:
+        computation['yr2'] = arr1['yr2'] / arr2['yr2'],
+    except Exception as e:
+        computation['yr1'] = 0
+
+    try:
+        computation['yr3'] = arr1['yr3'] / arr2['yr3'],
+    except Exception as e:
+        computation['yr3'] = 0
+
+    return computation
 
 
 register = template.Library()
