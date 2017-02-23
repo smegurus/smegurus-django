c517bae1d4e419efdfb0f4e8f568ee715d54416c
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-22 20:04:48 -0500

    Added ability to delete organization.

--- foundation_public/management/commands/delete_organization.py
+++ foundation_public/management/commands/delete_organization.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection # Used for django tenants.
 from django.utils.translation import ugettext_lazy as _
@@ -20,21 +21,11 @@ class Command(BaseCommand):
         # the public schema." error.
         connection.set_schema_to_public()
 
-        # Fetch our registered Organization.
+        # Fetch our registered Organization per "schema_name" and delete.
         for schema_name in options['schema_name']:
             try:
-                # Fetch the organization.
                 organization = PublicOrganization.objects.get(schema_name__iexact=schema_name)
-
-                # Delete all the users in that organization.
-                # users = organization.users
-                # for user in users.all():
-                #     user.delete()
-
-                # Delete the tenant.
                 organization.delete()
-
-                # Provide successful message.
                 self.stdout.write(
                     self.style.SUCCESS(_('Successfully deleted organization.'))
                 )
