e19b6dcc074262764e07414adce4387f6c73c239
Bartlomiej Mika <bart@mikasoftware.com>
2017-02-27 22:41:49 -0500

    Added new command.

---
+++ foundation_public/management/commands/list_user.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection # Used for django tenants.
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = _('Activates a user.')
+
+    def handle(self, *args, **options):
+        # Connection needs first to be at the public schema, as this is where
+        # the database needs to be set before creating a new tenant. If this is
+        # not done then django-tenants will raise a "Can't create tenant outside
+        # the public schema." error.
+        connection.set_schema_to_public() # Switch to Public.
+
+        users = User.objects.all()
+        for user in users.all():
+            self.stdout.write(
+                self.style.SUCCESS("ID "+user.get_full_name()+" - "+str(user)+" - "+user.email)
+            )
