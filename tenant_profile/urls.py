02f116e7e9993ea4fb0b49e29371d7c3fbee0c3f
Kanwarpreet <fairo4544mandy@gmail.com>
2017-08-09 13:30:48 +0000

    Issues #904, #905, #906, #907, #908, #909, #910 and #911 urls fixed Update urls.py

--- tenant_profile/urls.py
+++ tenant_profile/urls.py
@@ -13,11 +13,12 @@ urlpatterns = (
     url(r'^is_locked$', me_views.tenant_profile_is_locked_page, name='tenant_profile_is_locked'),
 
     # Organization Views
-    url(r'^settings/organization/profile$', org_views.profile_settings_profile_page, name='tenant_profile_setting_profile'),
-    url(r'^settings/organization/address$', org_views.profile_settings_address_page, name='tenant_profile_setting_address'),
-    url(r'^settings/organization/program_tags$', org_views.profile_settings_address_page, name='tenant_profile_setting_program_tags'),
-    url(r'^settings/organization/client_tags$', org_views.profile_settings_address_page, name='tenant_profile_setting_client_tags'),
-    url(r'^settings/organization/perks$', org_views.profile_settings_address_page, name='tenant_profile_setting_perks'),
-    url(r'^settings/organization/affiliate_links$', org_views.profile_settings_address_page, name='tenant_profile_setting_affiliate_links'),
+    url(r'^settings/organization/profile$', org_views.profile_settings_profile_page, name='tenant_profile_setting_organization_profile'),
+    url(r'^settings/organization/address$', org_views.profile_settings_address_page, name='tenant_profile_setting_organization_address'),
+    url(r'^settings/organization/preferences$', org_views.profile_settings_preferences_page, name='tenant_profile_setting_organization_preferences'),
+    url(r'^settings/organization/program_tags$', org_views.profile_settings_program_tags_page, name='tenant_profile_setting_organization_program_tags'),
+    url(r'^settings/organization/client_tags$', org_views.profile_settings_client_tags_page, name='tenant_profile_setting_organization_client_tags'),
+    url(r'^settings/organization/perks$', org_views.profile_settings_perks_page, name='tenant_profile_setting_organization_perks'),
+    url(r'^settings/organization/affiliate_links$', org_views.profile_settings_affiliate_links_page, name='tenant_profile_setting_organization_affiliate_links'),
 
 )
